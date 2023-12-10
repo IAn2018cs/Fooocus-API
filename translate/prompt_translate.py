0383d648093110863a902d60e56904132095a4f3
chenshuai <chenshuai@ian2018.cn>
2023-12-10 10:54:17 +0800

    fix prompt translate error

--- translate/prompt_translate.py
+++ translate/prompt_translate.py
@@ -15,11 +15,11 @@ def prompt_translate(prompt: str) -> str:
         source = result['lang']
         if source != 'en':
             print(f"[Prompt Translate] source is {source}, start translate")
-            prompt = mt.translate(prompt, source=source, target=dlt.lang.ENGLISH)
+            prompt = mt.translate(prompt, source=f'{source}', target=dlt.lang.ENGLISH)
         else:
             print("[Prompt Translate] source is ENGLISH, not translate")
-    except:
-        print("[Prompt Translate] translate error, use origin prompt")
+    except Exception as e:
+        print(f"[Prompt Translate] translate error: {e}, use origin prompt")
     finally:
         execution_time = time.perf_counter() - execution_start_time
         print(f'[Prompt Translate] Translate time: {execution_time:.2f} seconds')
